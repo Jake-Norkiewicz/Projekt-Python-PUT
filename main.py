b2e3f21510c0578255793375413cf38569de1f9a
Jake-Norkiewicz <150198737+Jake-Norkiewicz@users.noreply.github.com>
2023-12-11 01:05:42 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random
+import math
 
 def Fib_number(n = 0):
 
@@ -9,6 +11,55 @@ def Fib_number(n = 0):
         return 1
 
     return Fib_number(n-1) + Fib_number(n-2)
+
+
+def mean_value(numbers):
+
+    sum = 0
+    num_of_elements = len(numbers)
+
+    for i in range(0, num_of_elements):
+        sum += numbers[i]
+
+    mean = sum / num_of_elements
+
+    return float(mean)
+
+def standard_deviation(numbers):
+
+    mean = mean_value(numbers)
+    sum_of_squares_of_differences = 0
+
+    for i in range(0, len(numbers)):
+        diff = numbers[i] - mean
+        sum_of_squares_of_differences += math.pow(diff, 2)
+
+    stan_dev = math.sqrt(sum_of_squares_of_differences/len(numbers))
+
+    return float(stan_dev)
+
+
+def max_value(numbers):
+
+    max_element = numbers[0]    #we're assuming that the first element is the greatest one. We have to do this assumption because we are going to compare this value with the rest and overwrite it if needed
+
+    for i in range(0, len(numbers)):
+        if max_element < numbers[i]:    #if there's a greater element in the list - overwrite the maximum element value
+            max_element = numbers[i]
+
+    return max_element
+
+
+def min_value(numbers):
+
+    min_element = numbers[0]    #same assumption as in the max_value() function
+
+    for i in range(0, len(numbers)):
+        if min_element > numbers[i]:
+            min_element = numbers[i]
+
+    return min_element
+
 
 def ex_1():
 
@@ -33,18 +84,49 @@ def ex_1():
 
 
 def ex_2():
-    pass
+
+    print("Enter how many random numbers to save in the file:")
+    n = int(input())
+    numbers = []
+
+    for i in range(n):
+        random_number = random.randint(0, 99)
+        numbers.append(random_number)
+
+    path = "ex_2_file.txt"
+    with open(path, 'w') as file:
+        for i in range(0, n):
+            file.write(str(numbers[i]))
+            file.write('\n')
+
+    print("Generated random number list:", numbers, "has been saved in the file", path, "successfully")
 
 def ex_3():
-    pass
+
+    numbers = []
+
+    path = "ex_2_file.txt"
+    with open(path, 'r') as file:
+        all_lines = file.readlines()
+
+        for line in all_lines:
+            x = int(line.strip())
+            numbers.append(x)
+
+    print("Numbers from the file", path, ":", numbers)
+
+    print("Mean:", mean_value(numbers))
+    print("Standard deviation:", standard_deviation(numbers))
+    print("Maximum value:", max_value(numbers))
+    print("Minimum value:", min_value(numbers))
 
 def ex_4():
 
-    print("Input a positive integer n: ")
+    print("Input a positive integer n:")
     n = int(input())
 
     result = Fib_number(n)
-    print(result)
+    print("n-th term of Fibonacci sequence is: ", result)
 
 def ex_5():
     pass
